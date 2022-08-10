b7549e4983a6ab3537a9c80dfc46c67d746b2d77
César Palma Morante <cemp2703@gmail.com>
2022-08-10 14:56:56 -0500

    fix header background

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export function Default({ children }: Props) {
   return (<>
-  <header className='h-40 bg-bg-header-mobile bg-primary lg:bg-bg-header'></header>
+  <header className='h-40 bg-header-mobile bg-primary lg:bg-header-desktop'></header>
     <main className='flex flex-col gap-14 relative lg:gap-10 pb-9 lg:pb-32'>{children}</main>
   </>
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
-      textColor:{
-        "primary": "hsl(180, 29%, 50%)",
-        "secondary": "hsl(180, 14%, 20%)",
-        "tertiary": "hsl(180, 8%, 52%)",
-        "background":"hsl(180, 52%, 96%)",
-        "filter-tables":"hsl(180, 31%, 95%)"
+      textColor: {
+        primary: 'hsl(180, 29%, 50%)',
+        secondary: 'hsl(180, 14%, 20%)',
+        tertiary: 'hsl(180, 8%, 52%)',
+        background: 'hsl(180, 52%, 96%)',
+        'filter-tables': 'hsl(180, 31%, 95%)'
       },
       backgroundColor: {
-        "primary": "hsl(180, 29%, 50%)",
-        "secondary": "hsl(180, 14%, 20%)",
-        "tertiary": "hsl(180, 8%, 52%)",
-        "background":"hsl(180, 52%, 96%)",
-        "filter-tables":"hsl(180, 31%, 95%)"
+        primary: 'hsl(180, 29%, 50%)',
+        secondary: 'hsl(180, 14%, 20%)',
+        tertiary: 'hsl(180, 8%, 52%)',
+        background: 'hsl(180, 52%, 96%)',
+        'filter-tables': 'hsl(180, 31%, 95%)'
       },
-      fontFamily:{
+      fontFamily: {
         'League Spartan': ['League Spartan', 'sans-serif']
       },
-      fontSize:{
-        'base': '0.9375rem',
+      fontSize: {
+        base: '0.9375rem'
       },
-      backgroundImage:{
-        'bg-header': "url('./images/bg-header-desktop.svg')",
-        'bg-header-mobile': "url('./images/bg-header-mobile.svg')",
+      backgroundImage: {
+        'header-desktop': "url('./images/bg-header-desktop.svg')",
+        'header-mobile': "url('./images/bg-header-mobile.svg')"
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
